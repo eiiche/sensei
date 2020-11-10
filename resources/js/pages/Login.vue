aaf9338befc291c52c905b00383d2ac9bad22e08
ynaka6 <yuuki.nakamura.0828@gmail.com>
2020-11-10 10:16:46 +0800

    AjaxやAPIのサンプルを用意しました

--- resources/js/pages/Login.vue
+++ resources/js/pages/Login.vue
@@ -14,7 +14,7 @@
                         <b-nav-item to="/register">新規登録</b-nav-item>
                     </b-nav>
                     <b-card>
-                        <b-form>
+                        <b-form @submit.prevent="onSubmit">
                             <b-form-group
                                 label="メールアドレス"
                                 label-for="email"
@@ -25,14 +25,21 @@
                                     type="email"
                                     required
                                 ></b-form-input>
+                                <b-form-invalid-feedback :state="!formState('email')">
+                                    {{ formState('email') }}
+                                </b-form-invalid-feedback>
                             </b-form-group>
 
                             <b-form-group label="パスワード" label-for="password">
                                 <b-form-input
                                     id="password"
+                                    type="password"
                                     v-model="form.password"
                                     required
                                 ></b-form-input>
+                                <b-form-invalid-feedback :state="!formState('password')">
+                                    {{ formState('password') }}
+                                </b-form-invalid-feedback>
                             </b-form-group>
 
 
@@ -54,8 +61,30 @@ export default {
             form: {
                 email: null,
                 password: null,
-            }
+            },
+            errors: {}
         }
-    }
+    },
+    methods: {
+        formState(name) {
+            return this.errors && this.errors[name] && 0 < this.errors[name].length
+                ? this.errors[name][0]
+                : ''
+        },
+        onSubmit() {
+            this.$store
+                .dispatch('auth/login', this.form)
+                .then(() => {
+                    this.$router.push('/')
+                })
+                .catch((err) => {
+                    const response = err.response
+                    const errors = response.data.errors
+                    if (errors) {
+                        this.errors = errors
+                    }
+                })
+        },
+    },
 }
 </script>
